b6b97a85f15d3593878f650351b2d04aaf1aa985
Crowdbotics <engineering@crowdbotics.com>
2022-07-22 09:08:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/newtime2_dev_26463/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newtime2_dev_26463;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "newtime2_dev_26463";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "newtime2_dev_26463",
+  url: "https://newtime2_dev_26463.botics.co",
+  api: "https://newtime2_dev_26463.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
